2bc0c55d9027e27a48ea56612971a69ddd647329
kenny-ihi <kenny@ironhorse.io>
2019-02-01 12:17:54 -0800

    strips state out of recent projects and converts data to a constant

--- src/components/RecentProjects/RecentProjects.js
+++ src/components/RecentProjects/RecentProjects.js
@@ -1,37 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import techdecoded_thumb from './img/tech-decoded-thumb.jpg';
 import './RecentProjects.css';
 
-class RecentProjects extends Component {
-  state = {
-    projects: [
-      {url: 'https://google.com', thumb: techdecoded_thumb, name: 'Intel® Tech Decoded', alt: 'Tech Decoded', id: 1},
-      {url: 'https://google.com', thumb: techdecoded_thumb, name: 'Intel® Tech Decoded', alt: 'Tech Decoded', id: 2},
-      {url: 'https://google.com', thumb: techdecoded_thumb, name: 'Intel® Tech Decoded', alt: 'Tech Decoded', id: 3},
-      {url: 'https://google.com', thumb: techdecoded_thumb, name: 'Intel® Tech Decoded', alt: 'Tech Decoded', id: 4},
-      {url: 'https://google.com', thumb: techdecoded_thumb, name: 'Intel® Tech Decoded', alt: 'Tech Decoded', id: 5},
-      {url: 'https://google.com', thumb: techdecoded_thumb, name: 'Intel® Tech Decoded', alt: 'Tech Decoded', id: 6}
-    ],
-  }
-  render() {
-    return(
-      <div id="recentprojects">
-        <h2>RECENT PROJECTS</h2>
-        <div id="projects">
-          {this.state.projects.map(project => {
-            return(
-              <div className="project" key={project.id}>
-                <a href={project.url}>
-                  <img width="189" height="189" src={project.thumb} alt={project.alt} />
-                </a>
-                <p className="home-project-title">{project.name}</p>
-              </div>
-            );
-          })}
-        </div>
+const projects = [
+  {url: 'https://google.com', thumb: techdecoded_thumb, name: 'Intel® Tech Decoded', alt: 'Tech Decoded', id: 1},
+  {url: 'https://google.com', thumb: techdecoded_thumb, name: 'Intel® Tech Decoded', alt: 'Tech Decoded', id: 2},
+  {url: 'https://google.com', thumb: techdecoded_thumb, name: 'Intel® Tech Decoded', alt: 'Tech Decoded', id: 3},
+  {url: 'https://google.com', thumb: techdecoded_thumb, name: 'Intel® Tech Decoded', alt: 'Tech Decoded', id: 4},
+  {url: 'https://google.com', thumb: techdecoded_thumb, name: 'Intel® Tech Decoded', alt: 'Tech Decoded', id: 5},
+  {url: 'https://google.com', thumb: techdecoded_thumb, name: 'Intel® Tech Decoded', alt: 'Tech Decoded', id: 6}
+]
+const recentProjects = () => {
+  return(
+    <div id="recentprojects">
+      <h2>RECENT PROJECTS</h2>
+      <div id="projects">
+        {projects.map(project => {
+          return(
+            <div className="project" key={project.id}>
+              <a href={project.url}>
+                <img width="189" height="189" src={project.thumb} alt={project.alt} />
+              </a>
+              <p className="home-project-title">{project.name}</p>
+            </div>
+          );
+        })}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default RecentProjects;
+export default recentProjects;
